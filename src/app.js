f531c107970224facaab01dd6ade7b078ab32ecb
Lorenzo Rodigliano <rodiglianolorenzo@gmail.com>
2026-04-08 12:06:27 +0200

    fix: reorder app initialization in app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
+const app = express();
 const postsRoutes = require('../routes/posts');
 
-const app = express();
 const port = 3000;
 
 app.use(express.json());
